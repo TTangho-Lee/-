bc57e22892400337a20ef9629ffe2fa856737727
이승호 <163648462+TTangho-Lee@users.noreply.github.com>
2024-08-21 18:23:55 +0900

    Update b.java

--- java_test/b.java
+++ java_test/b.java
@@ -3,8 +3,21 @@ package java_test;
 public class b {
     public static void main(String[] args) {
 
-        // 이 부분에 code를 작성해주세요!
-
+        Scanner scanner = new Scanner(System.in);
+        String input2 = scanner.nextLine();
+        String[] array={"a","e","i","o","u"};
+        boolean tmp=false;
+        for(int i=0;i<5;i++){
+            if (input2.equals(array[i])) {
+                tmp = true;
+                break;
+            }
+        }
+        if(tmp){
+            System.out.println("O");
+        }else{
+            System.out.println("X");
+        }
     }
 
 }

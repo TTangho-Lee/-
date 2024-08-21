c0813b6eee840d25003ff8970dc294e12102f6d8
이승호 <163648462+TTangho-Lee@users.noreply.github.com>
2024-08-21 18:25:32 +0900

    Update a.java

--- java_test/a.java
+++ java_test/a.java
@@ -1,10 +1,9 @@
 package java_test;
-
+import java.util.Scanner;
 public class a {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String input1 = scanner.nextLine();
         System.out.println(new StringBuffer(input1).reverse());
     }
-
 }
